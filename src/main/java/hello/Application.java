45771559c5247ae298939b2d3fecff24091338e8
Dmitry Mylnev <Dmitry.Mylnev@Markets.com>
2017-06-05 14:14:50 +0300

    Working example

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by dmylnev on 05.06.2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
